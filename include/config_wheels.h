dee4c4ed5156d0fd03e3305499a76c4dbc8fb534
moonlight169 <monlight169@gmail.com>
2026-07-10 21:11:45 +0700

    feat(config): configure pin mappings for 4-wheel motor drive

--- include/config_wheels.h
+++ include/config_wheels.h
@@ -1,8 +1,26 @@
-#define motor1_A PB1
-#define motor1_B PB0
+#define MotorFL_A PA0
+#define MotorFL_B PA1
 
-#define enc_A PB13
-#define enc_B PB12
+#define MotorFR_A PA2
+#define MotorFR_B PA3
+
+#define MotorRL_A PA6
+#define MotorRL_B PA7
+
+#define MotorRR_A PB0
+#define MotorRR_B PB1
+
+#define EncFL_A PB6
+#define EncFL_B PB7
+
+#define EncFR_A PB4
+#define EncFR_B PB5
+
+#define EncRL_A PB14
+#define EncRL_B PB15
+
+#define EncRR_A PB13
+#define EncRR_B PB12
 
 #define MAX_RPM 300
 #define PULSE_PER_REV 844.8
--- src/slave_wheels/main.cpp
+++ src/slave_wheels/main.cpp
@@ -5,23 +5,23 @@
 #include "encoder.h"
 #include "wheel.h"
 
-Wheel wheel1(motor1_A, motor1_B, enc_A, enc_B, PULSE_PER_REV);
+Wheel wheelFL(MotorFL_A, MotorFL_B, EncFL_A, EncFL_B, PULSE_PER_REV);
 
-void isrFL_A() { wheel1.handleA(); }
-void isrFL_B() { wheel1.handleB(); }
+void isrFL_A() { wheelFL.handleA(); }
+void isrFL_B() { wheelFL.handleB(); }
 
 
 void setup(){
     Serial.begin(115200);
-    attachInterrupt(digitalPinToInterrupt(enc_A), isrFL_A, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(enc_B), isrFL_B, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(EncFL_A), isrFL_A, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(EncFL_B), isrFL_B, CHANGE);
 }
 
 void loop(){
-    wheel1.smoothRun(0);
+    wheelFL.smoothRun(0);
 
-    Serial.print("Count: "); Serial.print(wheel1.getCount());
-    Serial.print(" | RPM: "); Serial.println(wheel1.getRPM(), 1);
+    Serial.print("Count: "); Serial.print(wheelFL.getCount());
+    Serial.print(" | RPM: "); Serial.println(wheelFL.getRPM(), 1);
 
     delay(100);
 }
