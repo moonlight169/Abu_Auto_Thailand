a5485b8cf00b98722ad315d0d076192d4b5279ea
moonlight169 <monlight169@gmail.com>
2026-07-12 17:55:09 +0700

    add: sensor pin configuration header

---
+++ include/config_sensor.h
@@ -0,0 +1,21 @@
+#define Relay_1 PB12
+#define Relay_2 PB13
+#define Relay_3 PB14
+#define Relay_4 PB15
+
+#define Servo_1 PB4
+#define Servo_2 PB5
+
+#define Light_sensor_1 PB6
+#define Light_sensor_2 PB7
+
+#define L_SW_lift_fonttop_1 PB1
+#define L_SW_lift_fontbottom_3 PA6
+
+#define L_SW_lift_backtop_5 PA3
+#define L_SW_lift_backbottom_7 PA1
+
+#define L_SW_armback_2 PB0
+#define L_SW_armfont_4 PA7
+
+#define L_SW_fontrobot_6 PA2
